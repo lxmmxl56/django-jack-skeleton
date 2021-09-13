a3e202865b3fe84cd21d78cbc0f39d7e4933bcb5
Matthew Long <matthew.long@t-mark.co.jp>
2021-09-13 11:45:11 +0900

    UPDATE: Tweak content block containers, add some html closing tag signpost, add messages block for use when context isn't available due to a redirect, fix boostrap js map file

--- jack/views.py
+++ jack/views.py
@@ -2,7 +2,6 @@ from logging import getLogger
 from datetime import timedelta
 
 from django.conf import settings
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
